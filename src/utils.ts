c6f7bc66c95be29d74df5f58522c736bff5335de
King-3 <75344974+coderking3@users.noreply.github.com>
2025-08-19 09:04:10 +0000

    fix: Delete useless functions

--- src/utils.ts
+++ src/utils.ts
@@ -31,23 +31,3 @@ export function renameRules(
     })
   )
 }
-
-export function isInEditorEnv(): boolean {
-  if (process.env.CI) return false
-  if (isInGitHooksOrLintStaged()) return false
-  return !!(
-    process.env.VSCODE_PID ||
-    process.env.VSCODE_CWD ||
-    process.env.JETBRAINS_IDE ||
-    process.env.VIM ||
-    process.env.NVIM
-  )
-}
-
-export function isInGitHooksOrLintStaged(): boolean {
-  return !!(
-    process.env.GIT_PARAMS ||
-    process.env.VSCODE_GIT_COMMAND ||
-    process.env.npm_lifecycle_script?.startsWith('lint-staged')
-  )
-}
