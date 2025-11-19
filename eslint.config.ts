8d86874ac28f01dde5d44b9af6526b5fbf97c02d
King-3 <75344974+coderking3@users.noreply.github.com>
2025-11-19 07:32:38 +0000

    🔧 chore: standardize Prettier and ESLint configurations

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,34 @@
-import { king3 } from './src'
+import { king3 } from './src/index'
 
-const eslintConfig = king3({
-  pnpm: true,
-  react: true,
-  typescript: true,
-  unocss: true,
-  vue: true
-})
+const eslintConfig = king3(
+  {
+    vue: true,
+    react: true,
+    nextjs: false,
+    typescript: true,
+    markdown: {
+      overrides: {
+        'no-dupe-keys': 'off'
+      }
+    },
+    pnpm: true,
+    prettier: true
+  },
+  {
+    ignores: ['fixtures', '_fixtures', '**/constants-generated.ts']
+  },
+  {
+    files: ['src/**/*.ts'],
+    rules: {
+      'perfectionist/sort-objects': [
+        'error',
+        {
+          order: 'asc',
+          type: 'natural'
+        }
+      ]
+    }
+  }
+)
 
 export default eslintConfig
